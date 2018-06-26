e87e7a873dff83696ea1cf3b1dbe3cba322921c2
c-g-e-w-e-k-e- <christophergewecke@gmail.com>
2018-06-26 08:37:29 -0700

    Remove all reference to `sendAsync`
    + Make compatible with web3 1.0 provider

--- subproviders/provider.js
+++ subproviders/provider.js
@@ -9,12 +9,11 @@ inherits(ProviderSubprovider, Subprovider)
 
 function ProviderSubprovider(provider){
   if (!provider) throw new Error('ProviderSubprovider - no provider specified')
-  if (!provider.sendAsync) throw new Error('ProviderSubprovider - specified provider does not have a sendAsync method')
   this.provider = provider
 }
 
 ProviderSubprovider.prototype.handleRequest = function(payload, next, end){
-  this.provider.sendAsync(payload, function(err, response) {
+  this.provider.send(payload, function(err, response) {
     if (err) return end(err)
     if (response.error) return end(new Error(response.error.message))
     end(null, response.result)
